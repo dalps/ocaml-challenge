60f2ed7bcce002fd910e2c623be4b5737745ca6f
Federico <dalpio23@gmail.com>
2024-01-19 15:37:18 +0100

    Fix consecutive-even

--- 3/consecutive-even/solve.ml
+++ 3/consecutive-even/solve.ml
@@ -1,5 +1,8 @@
 let consecutive_even l =
   List.fold_left
-    (fun (c, m) a -> if a mod 2 = 0 then (1 + c, m) else (0, max c m))
+    (fun (c, m) a ->
+      let c' = if a mod 2 = 0 then 1 + c else 0 in
+      let m' = max c' m in
+      (c', m'))
     (0, 0) l
   |> snd
